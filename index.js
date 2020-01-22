120e34631cc4ea5d221d2c1a50c449621858bd2c
DentonWarnock <dentonwarnock@gmail.com>
2020-01-21 18:46:50 -0800

    initial commit. project setup and server running on port 8800

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-// code away!
+const server = require("./server");
+
+server.listen(8800, () => {
+  console.log("*** Server Running on http://localhost:8800 ***");
+});
--- server.js
+++ server.js
@@ -1,13 +1,22 @@
-const express = require('express');
+const express = require("express");
+const userRouter = require("./users/userRouter");
+const helmet = require("helmet");
+const morgan = require("morgan");
 
 const server = express();
+server.use(express.json(), helmet(), morgan("dev", logger));
 
-server.get('/', (req, res) => {
+server.use("/api/users", userRouter);
+
+server.get("/", (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
 });
 
 //custom middleware
 
-function logger(req, res, next) {}
+function logger(req, res, next) {
+  console.log(`${req.method} Request`);
+  next();
+}
 
 module.exports = server;
